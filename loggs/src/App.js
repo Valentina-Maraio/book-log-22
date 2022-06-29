24d4c34630d9799e9701babfb3873a66b898d5a3
Valentina Maraio <valentina.maraio89@gmail.com>
2022-06-29 17:36:30 +0200

    country json

--- loggs/src/App.js
+++ loggs/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Countries from './comps/Countries';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Countries />
       </header>
     </div>
   );
